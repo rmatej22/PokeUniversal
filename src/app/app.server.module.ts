085d27fdd6ab4c231bb1d109ba18dd1e122d4afb
rmatej22 <rmatej545@gmail.com>
2023-02-22 18:54:10 +0100

    add main.server & tsconfig.server & app.server.module

---
+++ src/app/app.server.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { ServerModule } from '@angular/platform-server';
+import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
+
+@NgModule({
+  imports: [AppModule, ServerModule],
+  declarations: [],
+  bootstrap: [AppComponent],
+})
+export class AppServerModule {}
