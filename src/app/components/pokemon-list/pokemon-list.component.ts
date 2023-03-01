672c65491554413dad4811f80ad71c8e0758b2cd
rmatej22 <rmatej545@gmail.com>
2023-03-01 19:47:39 +0100

    add logic for displaying pokemon-list

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Pokemon } from 'src/app/models/pokemon.model';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.css']
+  styleUrls: ['./pokemon-list.component.css'],
 })
 export class PokemonListComponent implements OnInit {
+  pokemonData$!: Observable<Pokemon[]>;
 
-  constructor() { }
+  constructor(private pokemonService: PokemonService) {}
 
   ngOnInit(): void {
+    this.pokemonData$ = this.pokemonService.getPokemons();
   }
-
 }
