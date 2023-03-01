821f695a84e1a048f8db0b320f1e5e888d016eff
rmatej22 <rmatej545@gmail.com>
2023-03-01 19:00:10 +0100

    create pokemon-details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { PokemonDetailsComponent } from './components/pokemon-details/pokemon-details.component';
 
 @NgModule({
-  declarations: [AppComponent, PokemonListComponent],
+  declarations: [AppComponent, PokemonListComponent, PokemonDetailsComponent],
   imports: [BrowserModule.withServerTransition({ appId: 'serverApp' })],
   providers: [],
   bootstrap: [AppComponent],
