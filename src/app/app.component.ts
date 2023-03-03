9327cc37ceb17ff0d3348d8cbde6a6f1187ed5d2
rmatej22 <rmatej545@gmail.com>
2023-03-03 15:48:22 +0100

    set title and meta tags for app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'poke-universal';
+export class AppComponent implements OnInit {
+  constructor(private title: Title, private meta: Meta) {}
+
+  ngOnInit(): void {
+    this.title.setTitle('Explore Pokemons');
+
+    this.meta.addTags([
+      {
+        name: 'author',
+        content: 'Matej',
+      },
+      {
+        name: 'keywords',
+        content: 'angular-universal-seo',
+      },
+      {
+        name: 'description',
+        content: 'Simple Angular application with SSR.',
+      },
+    ]);
+  }
 }
